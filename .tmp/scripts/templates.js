4792ec8baa506bd7d8e6b83dc2209f38a82a462b
Alex Codreanu <alexcodreanu86@gmail.com>
2014-07-12 15:22:37 -0500

    recompiled

--- .tmp/scripts/templates.js
+++ .tmp/scripts/templates.js
@@ -5,7 +5,7 @@
     function Templates() {}
 
     Templates.renderForm = function() {
-      return _.template("<div class='widget' data-id=\"stock-widget-wrapper\">\n  <div class='widget-header'>\n    <h2 class=\"widget-title\">Stock</h2>\n    <div class=\"widget-form\" data-id=\"stock-form\">\n      <input name=\"stock-search\" type=\"text\">\n      <button data-id=\"stock-button\">Get Stock Data</button><br>\n    </div>\n  </div>\n  <div data-id=\"stock-output\"></div>\n</div>");
+      return _.template("<div class='widget' data-id=\"stock-widget-wrapper\">\n  <div class='widget-header'>\n    <h2 class=\"widget-title\">Stock</h2>\n    <div class=\"widget-form\" data-id=\"stock-form\">\n      <input name=\"stock-search\" type=\"text\">\n      <button data-id=\"stock-button\">Get Stock Data</button><br>\n    </div>\n  </div>\n  <div class=\"widget-body\" data-id=\"stock-output\"></div>\n</div>");
     };
 
     Templates.renderLogo = function(imgData) {
